ef266ba58908dfeadbe80421ca7b7ed740c635fb
geon lee <geon.lee@navercorp.com>
2020-09-10 01:10:48 +0900

    next.js basic Assets, Metadata, and CSS

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>안녕하세요, 저는 이건입니다.</p>
+      </section>
+    </Layout>
+  )
+}
